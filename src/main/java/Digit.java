dd3607e64e5d314509ce94b92604bd6bcc304acf
Vlasiuk_Andrii <vlasiuk.andrii93@gmail.com>
2016-11-23 21:24:18 +0200

    initaial commit

---
+++ src/main/java/Digit.java
@@ -0,0 +1,75 @@
+/**
+ ._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+ |.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+ |_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
+ */
+public class Digit {
+    private String n0 = "._.\n|.|\n|_|";
+    private String n1 = "...\n..|\n..|";
+    private String n2 = "._.\n._|\n|_.";
+    private String n3 = "._.\n._|\n._|";
+
+    int[] items =  new int [3];
+    char[] itemsChar = new char [3];
+    String [] strings = new String [] {n0,n1,n2,n3};
+
+    public String printDigits(int value) {
+        itemsChar = ("" + value).toCharArray();
+        for(int i = itemsChar.length; i > 0; i--){
+            items[itemsChar.length - i] = Character.getNumericValue(itemsChar[i - 1]);
+        }
+
+//        if (index > 99) {
+//            items[0] = index % 10;
+//            items[1] = ((index - items[0]) / 10) % 10;
+//            items[2] = (index - items[0]) / 100;
+//            return printDigits(items[2], items[1], items[0]);
+//        }
+//        if (index > 9) {
+//            items[0] = index % 10;
+//            items[1] = (index - items[0]) / 10;
+//            return printDigits(items[1], items[0]);
+//        }
+
+        return "";
+    }
+
+    public String printDigits(int index0, int index1) {
+        return makeFirstLine(index0,index1) + "\n" + makeSecondLine(index0, index1) + "\n" + makeThirdLine(index0, index1);
+    }
+
+    private String makeFirstLine(int i0, int i1){
+        return String.valueOf(strings[i0].split("\n")[0]) + " " + String.valueOf(strings[i1].split("\n")[0]);
+    }
+    private String makeSecondLine(int i0, int i1){
+        return String.valueOf(strings[i0].split("\n")[1]) + " " + String.valueOf(strings[i1].split("\n")[1]);
+    }
+    private String makeThirdLine(int i0, int i1){
+        return String.valueOf(strings[i0].split("\n")[2]) + " " + String.valueOf(strings[i1].split("\n")[2]);
+    }
+
+
+
+
+    public String printDigits(int index0, int index1, int index2) {
+        return makeFirstLine(index0,index1, index2) + "\n"
+                + makeSecondLine(index0, index1, index2) + "\n"
+                + makeThirdLine(index0, index1, index2);
+    }
+    private String makeFirstLine(int i0, int i1, int i2){
+        return String.valueOf(strings[i0].split("\n")[0]) + " "
+                + String.valueOf(strings[i1].split("\n")[0] + " "
+                + String.valueOf(strings[i2].split("\n")[0]));
+    }
+    private String makeSecondLine(int i0, int i1, int i2){
+        return String.valueOf(strings[i0].split("\n")[1]) + " "
+                + String.valueOf(strings[i1].split("\n")[1]) + " "
+                + String.valueOf(strings[i2].split("\n")[1]);
+    }
+    private String makeThirdLine(int i0, int i1, int i2){
+        return String.valueOf(strings[i0].split("\n")[2]) + " "
+                + String.valueOf(strings[i1].split("\n")[2]) + " "
+                + String.valueOf(strings[i2].split("\n")[2]);
+    }
+
+}
---
+++ src/test/java/DigitTest.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class DigitTest {
+
+    Digit digit = new Digit();
+
+    @Test
+    public void emptyInput(){
+//        assertEquals("._.\n|.|\n|_|", digit.printDigits(0));
+//        assertEquals("...\n..|\n..|", digit.printDigits(1));
+//        assertEquals("._.\n._|\n|_.", digit.printDigits(2));
+//        assertEquals("._.\n._|\n._|", digit.printDigits(3));
+//
+//        //assertEquals("... ._.\n..| ._|\n..| |_.", digit.printDigits(1,2));
+//        assertEquals("... ._.\n..| ._|\n..| |_.", digit.printDigits(12));
+//        assertEquals("._. ._.\n._| ._|\n._| |_.", digit.printDigits(32));
+
+        assertEquals("._. ._. ._.\n._| ._| ._|\n._| |_. |_.", digit.printDigits(659));
+        assertEquals("._. ._. ...\n._| ._| ..|\n._| |_. ..|", digit.printDigits(321));
+    }
+
+
+}
