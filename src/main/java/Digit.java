a3c922e6a4f793eabb0af27ded5bc1caca460891
Vlasiuk_Andrii <vlasiuk.andrii93@gmail.com>
2016-11-23 23:55:01 +0200

    Ready program till 10 numbers in input

--- src/main/java/Digit.java
+++ src/main/java/Digit.java
@@ -8,68 +8,42 @@ public class Digit {
     private String n1 = "...\n..|\n..|";
     private String n2 = "._.\n._|\n|_.";
     private String n3 = "._.\n._|\n._|";
+    private String n4 = "...\n|_|\n..|";
+    private String n5 = "._.\n|_.\n._|";
+    private String n6 = "._.\n|_.\n._|";
+    private String n7 = "._.\n|_.\n._|";
+    private String n8 = "._.\n|_.\n._|";
+    private String n9 = "._.\n|_.\n._|";
+    String result = "";
 
-    int[] items =  new int [3];
-    char[] itemsChar = new char [3];
-    String [] strings = new String [] {n0,n1,n2,n3};
+    int[] items =  new int [10];
+    static char[] itemsChar = new char [10];
+
+    void fillItemsChar(){
+        for(int k=0; k < itemsChar.length -1; k++){
+            itemsChar[k] = 'a';
+        }
+    }
+
+    String [] strings = new String [] {n0,n1,n2,n3,n4,n5,n6,n7,n8,n9};
 
     public String printDigits(int value) {
+        fillItemsChar();
         itemsChar = ("" + value).toCharArray();
-        for(int i = itemsChar.length; i > 0; i--){
-            items[itemsChar.length - i] = Character.getNumericValue(itemsChar[i - 1]);
+        for(int j = itemsChar.length; j > 0; j--){
+            items[itemsChar.length - j] = Character.getNumericValue(itemsChar[j - 1]);
         }
-
-//        if (index > 99) {
-//            items[0] = index % 10;
-//            items[1] = ((index - items[0]) / 10) % 10;
-//            items[2] = (index - items[0]) / 100;
-//            return printDigits(items[2], items[1], items[0]);
-//        }
-//        if (index > 9) {
-//            items[0] = index % 10;
-//            items[1] = (index - items[0]) / 10;
-//            return printDigits(items[1], items[0]);
-//        }
-
-        return "";
+        for(int line = 0; line < 3; line++){
+            for (int i = 0; i <= itemsChar.length -1; i++ ){
+                if(  itemsChar[i] != 'a'){
+                   result += String.valueOf(strings[ Character.getNumericValue(itemsChar[i])].split("\n")[line]);
+                    if ( i != itemsChar.length - 1)
+                        result += " ";
+                }
+            }
+            if (line != 2)
+                result += "\n";
+        }
+        return result;
     }
-
-    public String printDigits(int index0, int index1) {
-        return makeFirstLine(index0,index1) + "\n" + makeSecondLine(index0, index1) + "\n" + makeThirdLine(index0, index1);
-    }
-
-    private String makeFirstLine(int i0, int i1){
-        return String.valueOf(strings[i0].split("\n")[0]) + " " + String.valueOf(strings[i1].split("\n")[0]);
-    }
-    private String makeSecondLine(int i0, int i1){
-        return String.valueOf(strings[i0].split("\n")[1]) + " " + String.valueOf(strings[i1].split("\n")[1]);
-    }
-    private String makeThirdLine(int i0, int i1){
-        return String.valueOf(strings[i0].split("\n")[2]) + " " + String.valueOf(strings[i1].split("\n")[2]);
-    }
-
-
-
-
-    public String printDigits(int index0, int index1, int index2) {
-        return makeFirstLine(index0,index1, index2) + "\n"
-                + makeSecondLine(index0, index1, index2) + "\n"
-                + makeThirdLine(index0, index1, index2);
-    }
-    private String makeFirstLine(int i0, int i1, int i2){
-        return String.valueOf(strings[i0].split("\n")[0]) + " "
-                + String.valueOf(strings[i1].split("\n")[0] + " "
-                + String.valueOf(strings[i2].split("\n")[0]));
-    }
-    private String makeSecondLine(int i0, int i1, int i2){
-        return String.valueOf(strings[i0].split("\n")[1]) + " "
-                + String.valueOf(strings[i1].split("\n")[1]) + " "
-                + String.valueOf(strings[i2].split("\n")[1]);
-    }
-    private String makeThirdLine(int i0, int i1, int i2){
-        return String.valueOf(strings[i0].split("\n")[2]) + " "
-                + String.valueOf(strings[i1].split("\n")[2]) + " "
-                + String.valueOf(strings[i2].split("\n")[2]);
-    }
-
 }
--- src/test/java/DigitTest.java
+++ src/test/java/DigitTest.java
@@ -1,7 +1,12 @@
 import org.junit.Assert;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+/**
+ ._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+ |.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+ |_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
+ */
 
 
 public class DigitTest {
@@ -14,14 +19,24 @@ public class DigitTest {
 //        assertEquals("...\n..|\n..|", digit.printDigits(1));
 //        assertEquals("._.\n._|\n|_.", digit.printDigits(2));
 //        assertEquals("._.\n._|\n._|", digit.printDigits(3));
-//
-//        //assertEquals("... ._.\n..| ._|\n..| |_.", digit.printDigits(1,2));
-//        assertEquals("... ._.\n..| ._|\n..| |_.", digit.printDigits(12));
-//        assertEquals("._. ._.\n._| ._|\n._| |_.", digit.printDigits(32));
 
-        assertEquals("._. ._. ._.\n._| ._| ._|\n._| |_. |_.", digit.printDigits(659));
         assertEquals("._. ._. ...\n._| ._| ..|\n._| |_. ..|", digit.printDigits(321));
     }
 
-
+    @Test
+    public void name(){
+        assertEquals("._.\n|.|\n|_|", digit.printDigits(0));
+    }
+    @Test
+    public void nasefme(){
+        assertEquals("... ._.\n..| ._|\n..| |_.", digit.printDigits(12));
+    }
+    @Test
+    public void nasfme(){
+        assertEquals("._. ._.\n._| ._|\n._| |_.", digit.printDigits(32));
+    }
+    @Test
+    public void nasfmesf(){
+        assertEquals("", digit.printDigits(1234567890));
+    }
 }
